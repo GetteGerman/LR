5d23c628d261e65b2e335d9ba40c9632ed7cc1d5
German <gettegerman@gmani.com>
2023-11-20 18:27:34 +0700

    бан

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, buba!");
+            Console.WriteLine("Hello, bандура!");
             Console.WriteLine("Hello, Lupa!");
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
